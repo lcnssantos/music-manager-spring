469fe5277cbe5c97ff5a1b6a8826d775f04b8314
Luciano Santos <luciano.ssants@gmail.com>
2021-03-02 23:28:03 -0300

    Fix cors

--- src/main/java/com/music/api/config/WebConfig.java
+++ src/main/java/com/music/api/config/WebConfig.java
@@ -4,11 +4,13 @@ import com.music.api.interceptors.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
