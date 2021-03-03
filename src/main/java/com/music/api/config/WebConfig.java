b2ec470ea1d0a1eedf0bc19e3a3186940dfbe31a
Luciano Santos <luciano.ssants@gmail.com>
2021-03-02 23:23:12 -0300

    fix cors

--- src/main/java/com/music/api/config/WebConfig.java
+++ src/main/java/com/music/api/config/WebConfig.java
@@ -29,6 +29,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
-        registry.addMapping("/**/**");
     }
 }
